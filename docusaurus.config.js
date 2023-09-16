3666b39c87afbd2a206c38fbbd1daf9601b46a13
Tommy Ludwig <8924140+shakuzen@users.noreply.github.com>
2023-09-16 14:44:13 +0900

    Setup gh-pages deployment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,15 +11,18 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://JapanFinance.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'JapanFinance', // Usually your GitHub org/user name.
+  projectName: 'JapanFinance.github.io', // Usually your repo name.
+
+  // GitHub Pages adds a trailing slash to Docusaurus URLs by default. It is recommended to set a trailingSlash config (true or false, not undefined).
+  trailingSlash: true,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -42,14 +45,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'github.com/JapanFinance/JapanFinance.github.io/tree/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
