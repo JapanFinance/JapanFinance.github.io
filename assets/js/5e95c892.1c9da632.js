b5f9fb1ddd25d1e90da169e7def33c3d4395c509
elliottback <elliottback@users.noreply.github.com>
2023-12-30 10:14:08 +0000

    deploy: a6ee942402ff2ead6b09c885b8d9ab1544f9fd77

---
+++ assets/js/5e95c892.1c9da632.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkjapanfinance_wiki=self.webpackChunkjapanfinance_wiki||[]).push([[9661],{3143:(e,a,n)=>{n.r(a),n.d(a,{default:()=>p});n(7294);var s=n(6905),r=n(6742),c=n(8015),i=n(8790),u=n(8128),k=n(5893);function p(e){return(0,k.jsx)(r.FG,{className:(0,s.Z)(c.k.wrapper.docsPages),children:(0,k.jsx)(u.Z,{children:(0,i.H)(e.route.routes)})})}}}]);
