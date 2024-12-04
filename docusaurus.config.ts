611052bc273746b6480f04d426c159df4cc0d3e3
Tommy Ludwig <8924140+shakuzen@users.noreply.github.com>
2024-12-04 23:18:19 +0900

    Faster builds with Docusaurus Faster
    Adds and enables the new "experimental but safe" Docusaurus Faster feature added in Docusaurus 3.6.
    
    See https://docusaurus.io/blog/releases/3.6

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -25,6 +25,11 @@ const config: Config = {
   onBrokenLinks: 'warn', // TODO switch to 'throw' after addressing broken links
   onBrokenMarkdownLinks: 'warn',
 
+  future: {
+    // See https://docusaurus.io/blog/releases/3.6
+    experimental_faster: true
+  },
+
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
